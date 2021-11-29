e853e7211e5a63190133aff6eff28445df833f1e
maurinElectroTextile <maurin@etextile.org>
2021-11-29 23:21:37 +0100

    Split index & serial.js

---
+++ docs/js/serial.js
@@ -0,0 +1,26 @@
+async function onConnectButtonClick() {
+
+    if ('serial' in navigator) {
+        try {
+        const port = await navigator.serial.requestPort();
+        await port.open({ baudRate: 230400 });
+        this.reader = port.readable.getReader();
+        this.writer = port.writable.getWriter();
+        }
+        catch (err) {
+            onsole.error('There was an error opening the serial port:', err);
+        }
+    }
+    else {
+        console.error('The Web serial API doesn\'t seem to be enabled in your browser.');
+    }
+}
+  
+// Read the port data
+port.on("open", () => {
+    console.log('serial port open');
+});
+
+parser.on('data', data =>{
+    console.log('got word from arduino:', data);
+});
