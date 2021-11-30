a60451e8bd49dde368f019dc510dc1cbc56a05b5
maurinElectroTextile <maurin@etextile.org>
2021-11-30 15:07:01 +0100

    cleanUp

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -1,17 +1,15 @@
 /*
 This is simple code to control and read from the Arduino in P5
+See : https://editor.p5js.org/lemio/sketches/fOBD_hn-4
 */
 
 function setup() {
   createCanvas(400, 400);
 }
 
-var i = 0;
-
 function draw() {
-  i++;
-  background(100*ledStatus);
-  rect(val1 / 2, val2/2, 10, 10);
+  background(100 * ledStatus);
+  rect(val1/2, val2/2, 10, 10);
   fill(255, 255, 255);
   textSize(30);
   text(val1, 60, 60)
