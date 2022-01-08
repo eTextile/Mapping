d2117d9bfeba9a63a8ae1d11b04410a3683a0f1a
maurinElectroTextile <maurin@etextile.org>
2022-01-08 17:16:46 +0100

    Added 3D mesh for e256 raw data vis

--- docs/js/sketch.js
+++ docs/js/sketch.js
@@ -1,28 +1,40 @@
-/*
-This is simple code to control and read from the Arduino in P5
-See : https://editor.p5js.org/lemio/sketches/fOBD_hn-4
-*/
+// https://stackoverflow.com/questions/49318245/threejs-drag-points
 
-function setup() {
-  createCanvas(400, 400);
+var scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, -20, 10);
+camera.lookAt(scene.position);
+
+var renderer = new THREE.WebGLRenderer({
+  antialias: true
+});
+
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+var geometry = new THREE.PlaneBufferGeometry(15, 15, 15, 15);
+var plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({
+  wireframe: true,
+  color: "blue"
+}));
+scene.add(plane);
+
+var points = new THREE.Points(geometry, new THREE.PointsMaterial({
+  size: 0.1,
+  color: "red"
+}));
+scene.add(points);
+
+function updatePlane() {
+  for(let i = 0; i<256; i++){
+    geometry.attributes.position.setZ(i, e256_matrix.getZ(i+1));
+  }
+  geometry.attributes.position.needsUpdate = true;
 }
 
-function draw() {
-  background(255);
-  circle(width/2, height/2, 350);
+render();
 
-  //size(400, 400);
-  triangle(120, 300, 232, 80, 344, 300);
-  //rect(val1/2, val2/2, 10, 10)
-  //fill(255, 255, 255);
-  //textSize(30);
-  //text(val1, 60, 60);
-}
-
-function mouseClicked() {
-  //
-}
-
-function keyPressed() {
-  //
+function render() {
+  updatePlane();
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
 }
